ffa9e01746d1b173d9c9d04a220814aed7f6337d
Willianto Asalim <willianto@calibre.com.au>
2020-03-23 14:33:18 +1100

    Modified text

--- Plot1.R
+++ Plot1.R
@@ -4,6 +4,7 @@ download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_d
               , destfile = paste(path, "dataFiles.zip", sep = "/"))
 unzip(zipfile = "dataFiles.zip")
 
+# Load NEI & SCC Data
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
