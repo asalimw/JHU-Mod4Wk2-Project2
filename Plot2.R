d7edc3ade819282dd257dcab422bf1528bee129e
Willianto Asalim <willianto@calibre.com.au>
2020-03-23 14:33:42 +1100

    Plot 2 initial commit

---
+++ Plot2.R
@@ -0,0 +1,24 @@
+library("data.table")
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+
+# Load NEI & SCC Data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#Aggregate Using Sum the Baltimore Emission Data by Year
+aggTotBaltimore <- aggregate(Emissions ~ year, baltimoreNEI,sum)
+
+png("plot2.png",width=480,height=480,units="px",bg="transparent")
+
+barplot(
+  aggTotBaltimore$Emissions,
+  names.arg=aggTotBaltimore$year,
+  xlab="Year",
+  ylab="PM2.5 Emissions (Tons)",
+  main="Total PM2.5 Emissions From all Baltimore City Sources"
+)
+
+dev.off()
