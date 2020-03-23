b2f97dd9dec60c0230ded8aac2adf2c80dea434a
Willianto Asalim <willianto@calibre.com.au>
2020-03-23 17:36:13 +1100

    ggplot edit

--- Plot3.R
+++ Plot3.R
@@ -18,14 +18,12 @@ aggTotBaltimore <- aggregate(Emissions ~ year, baltimoreNEI,sum)
 
 png("plot3.png",width=480,height=480,units="px",bg="transparent")
 
-ggp <- ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
   geom_bar(stat="identity") +
   theme_bw() + guides(fill=FALSE)+
   facet_grid(.~type,scales = "free",space="free") + 
   labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
   labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
-
-print(ggp)
 
 dev.off()
 
