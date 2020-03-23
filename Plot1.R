a53be25124a17c48341942290e2f1506ca626136
Willianto Asalim <willianto@calibre.com.au>
2020-03-23 14:13:56 +1100

    plot 1 initial Commit

---
+++ Plot1.R
@@ -0,0 +1,23 @@
+library("data.table")
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+aggTotals <- aggregate(Emissions ~ year,NEI, sum)
+
+png("plot1.png",width=480,height=480,units="px",bg="transparent")
+
+barplot(
+  (aggTotals$Emissions)/10^6,
+  names.arg=aggTotals$year,
+  xlab="Year",
+  ylab="PM2.5 Emissions (10^6 Tons)",
+  main="Total PM2.5 Emissions From All US Sources Over The Years"
+)
+  
+dev.off()
+  
