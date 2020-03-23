df3ddc541f53bc3e6a601ab7db7b9ee4b4e7740d
Willianto Asalim <willianto@calibre.com.au>
2020-03-23 14:46:28 +1100

    Initial commit plot3

---
+++ Plot3.R
@@ -0,0 +1,31 @@
+library("data.table")
+library("ggplot2")
+
+#Set Path
+path <- getwd()
+
+#Download Data
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+
+# Load NEI & SCC Data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#Aggregate Using Sum the Baltimore Emission Data by Year
+aggTotBaltimore <- aggregate(Emissions ~ year, baltimoreNEI,sum)
+
+png("plot3.png",width=480,height=480,units="px",bg="transparent")
+
+ggp <- ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  theme_bw() + guides(fill=FALSE)+
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+print(ggp)
+
+dev.off()
+
